5995909ae1ae09ae9af070401d17950d8e0456db
西山海斗 <xishanhaidou@Kaito.local>
2017-06-28 14:01:22 +0900

    sample commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable, :confirmable, :omniauthable
   has_many :blogs
 
-#  mount_uploader :avatar, AvatarUploader
+  mount_uploader :avatar, AvatarUploader
 
   def self.find_for_facebook_oauth(auth, signed_in_resource=nil)
     user = User.find_by(email: auth.info.email)
