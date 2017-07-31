32a640e139a8c19a3f09a5b9697635771c863daa
西山海斗 <xishanhaidou@Kaito.local>
2017-07-31 16:05:15 +0900

    “20”

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,7 +76,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { host: 'https://arcane-badlands-80477.herokuapp.com/' }
+  config.action_mailer.default_url_options = { host: '　http://54.92.64.155/' }
   ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings =
 {
